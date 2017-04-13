a4a3a9a9140502cc5b5e31d112ab7b7ddc039a35
oleksandr.nikitin <oleksandr.nikitin@engsgepoint.com>
2017-04-13 18:46:06 +0300

    UPDATE: Set different coordinates to the circles

--- src/app/svg/svg.component.ts
+++ src/app/svg/svg.component.ts
@@ -12,10 +12,10 @@ import {
   selector: 'app-svg',
   styleUrls: ['./svg.component.css'],
   animations: [
-    trigger("fadeInOut", [
-      state("open", style({opacity: 1})),
-      state("closed", style({opacity: 0})),
-      transition("open <=> closed", animate( "3000ms" )),
+    trigger('fadeInOut', [
+      state('open', style({opacity: 1})),
+      state('closed', style({opacity: 0})),
+      transition('open <=> closed', animate( '3000ms' )),
     ])
   ],
   templateUrl: './svg.component.html',
@@ -27,9 +27,12 @@ export class SvgComponent implements OnInit {
     private clientY = 0;
     private setCurrentCx = undefined;
     private setCurrentCy =  undefined;
+    private setCurrentR =  undefined;
     public getPathD;
     public state = 'open';
     public timeOutRef;
+    public coordinatesCx: number[] = [100, 200];
+    public coordinatesCy: number[] = [100, 200];
 
     private onEvent(event: MouseEvent): void {
         this.event = event;
@@ -43,15 +46,25 @@ export class SvgComponent implements OnInit {
     // tslint:disable-next-line:member-ordering
     @Output() svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
-    constructor(private elementRef: ElementRef) {}
+    constructor(private elementRef: ElementRef) {
+        this.coordinatesCx = [100, 200];
+        this.coordinatesCy = [100, 200];
+    }
 
     clicked(event) {
         event.preventDefault();
         this.count++;
-        this.setCurrentCx = event.clientX;
-        this.setCurrentCy = event.clientY;
+        this.setCurrentCx = this.getRandomInt(event.clientX, event.clientX + 200);
+        this.setCurrentCy = this.getRandomInt(event.clientY, event.clientY + 300);
+        this.setCurrentR = this.getRandomInt(10, 100);
         this.setPathD();
         this.beginAnim();
+    }
+
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     private setPathD() {
@@ -59,7 +72,7 @@ export class SvgComponent implements OnInit {
         // svgElement.innerHTML="<circle>1</circle>";
         // console.log(svgElement);
 
-        return this.getPathD=`M256.5,377.8c0-12.4,8.4-21.2,20.2-21.2c11.8,0,19.5,8.7,19.8,21.2c0,11.8-7.7,20.9-20.2,20.9
+        return this.getPathD = `M256.5,377.8c0-12.4,8.4-21.2,20.2-21.2c11.8,0,19.5,8.7,19.8,21.2c0,11.8-7.7,20.9-20.2,20.9
         C264.6,398.6,256.5,389.5,256.5,377.8z M256.5,496.5c0-12.4,8.4-21.2,20.2-21.2c11.8,0,19.5,8.7,19.8,21.2
         c0,11.8-7.7,20.9-20.2,20.9C264.6,517.3,256.5,508.2,256.5,496.5`;
     }
