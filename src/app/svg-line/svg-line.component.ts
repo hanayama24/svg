073925937c7a7170ce95978871663b282e5efba6
Oleksandr Nikitin <canon.hanayama@gmail.com>
2017-04-21 18:14:23 +0300

    NEW: Added polygon coordinates

--- src/app/svg-line/svg-line.component.ts
+++ src/app/svg-line/svg-line.component.ts
@@ -7,6 +7,8 @@ import {
     EventEmitter
 } from '@angular/core';
 
+import polygonCoord from './polygon.const';
+
 @Component({
   selector: 'app-svg-line',
   templateUrl: './svg-line.component.html',
@@ -25,6 +27,23 @@ export class SvgLineComponent implements OnInit {
     private listEndX = [];
     private listEndY = [];
     public timeOutRef;
+
+    private startCoord = [
+        { x: 0, y: 0},
+        { x: 100, y: 100},
+    ];
+    private endCoord = [
+        { x: 100, y: 100},
+        { x: 200, y: 200},
+    ];
+
+    public polygonSX = [];
+    public polygonSY = [];
+    public polygonEX = [];
+    public polygonEY = [];
+
+    public polygonStartPoints = [];
+    public polygonEndPoints = [];
 
     @Output()
     coordinatesChanged = new EventEmitter();
@@ -56,7 +75,10 @@ export class SvgLineComponent implements OnInit {
         this.listEndY.push(this.setEndYCoord);
         this.listStartX.push(this.listEndX[this.listEndX.length - 1]);
         this.listStartY.push(this.listEndY[this.listEndY.length - 1]);
-        console.log('This list length end in: ', this.listEndX.length, this.listEndX);
+        this.endCoord.push({ x: this.clientX, y: this.clientY });
+        this.startCoord.push(this.endCoord[this.endCoord.length - 2]);
+        console.log('Start coordinate in object: ', this.endCoord, this.endCoord.length - 1 );
+        console.log('End coordinate in object: ', this.endCoord);
     }
 
     clearAllLines() {
@@ -67,8 +89,40 @@ export class SvgLineComponent implements OnInit {
         this.listStartY = [];
         this.setCurrentCx = [];
         this.setCurrentCy = [];
+        this.startCoord = [
+            {x: 100, y: 100}
+        ];
+        this.endCoord = [
+            {x: 0, y: 0}
+        ];
         this.coordinatesChanged.emit(this.setEndXCoord);
+    }
+
+    makePolygon() {
+        this.polygonSX = [231.5, 202.5, 202.5, 234.8, 285.8, 334.8, 388.3, 409.5, 388, 287.5];
+        this.polygonSY = [321, 369.8, 427, 477.6, 497.5, 497.5, 461.9, 397.8, 333.5, 294.6];
+
+        this.polygonEX = [202.5, 202.5, 234.8, 285.8, 334.8, 388.3, 409.5, 388, 287.5, 270.8, 213.9, 235.1];
+        this.polygonEY = [369.8, 427, 477.6, 497.5, 497.5, 461.9, 397.8, 333.5, 294.6, 329, 378.3, 451.1];
+
+        this.polygonStartPoints = polygonCoord;
+
+        this.polygonEndPoints = this.polygonStartPoints.slice(1);
+
+        this.endCoord = this.polygonEndPoints;
+        this.startCoord = this.polygonStartPoints;
+
+        this.startCoord.push(this.endCoord[this.endCoord.length - 2]);
+
+        this.listEndX = this.polygonEX;
+        this.listEndY = this.polygonEY;
+        this.listStartX = this.polygonSX;
+        this.listStartY = this.polygonSY;
     }
     ngOnInit() {
    }
 }
+
+
+
+
